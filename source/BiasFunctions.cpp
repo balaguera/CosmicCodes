ab86ab9a28eaf835fc973ebb1cd6b9838f698a14
Andrés Balaguera Antolínez <abalant@gmail.com>
2024-10-16 10:59:46 +0100

    Add files via upload

--- source/BiasFunctions.cpp
+++ source/BiasFunctions.cpp
@@ -15,11 +15,11 @@ real_prec MASS_BIAS_FUNCTIONS::mass_function(real_prec nu, real_prec z, void *p)
   struct s_CosmologicalParameters * s_cp= (struct s_CosmologicalParameters *)p;
   Cosmology cf(*s_cp);
   string mb=s_cp->mass_function_fit;
-  gsl_real A_par[10]={0};
-  gsl_real a_par[10]={0};
-  gsl_real b_par[10]={0};
-  gsl_real c_par[10]={0};
-  gsl_real D[10]={0};
+  vector<gsl_real> A_par(10,0);
+  vector<gsl_real> a_par(10,0);
+  vector<gsl_real> b_par(10,0);
+  vector<gsl_real> c_par(10,0);
+  vector<gsl_real> D(10,0);
   D[0]=0;
   D[1]=log10(200);
   D[2]=log10(300);
@@ -98,14 +98,14 @@ real_prec MASS_BIAS_FUNCTIONS::mass_function(real_prec nu, real_prec z, void *p)
       ans= 0.5*0.5800*(pow((real_prec)(deltac)/sqrt(nu),(real_prec)-1.37)+0.3)*exp(-1.036*nu*pow(deltac,(real_prec)-2));
   if(mb=="Tinker")
    {
-    AA=gsl_inter(D,A_par,9,log10((s_cp->Delta_SO)));
+    AA=gsl_inter_new(D,A_par,log10((s_cp->Delta_SO)));
     AA*=pow(1+z,-0.14);
-    a=gsl_inter(D,a_par,9,log10((s_cp->Delta_SO)));
+    a=gsl_inter_new(D,a_par,log10((s_cp->Delta_SO)));
     a*=pow(1+z,-0.06);
-    b=gsl_inter(D,b_par,9,log10((s_cp->Delta_SO)));
+    c=gsl_inter_new(D,c_par,log10((s_cp->Delta_SO)));
+    b=gsl_inter_new(D,b_par,log10((s_cp->Delta_SO)));
     b*=pow(1+z,-pow(10, -pow(0.75/log10((s_cp->Delta_SO)/75.0),1.2)));
-    c=gsl_inter(D,c_par,9,log10((s_cp->Delta_SO)));
-    ans= 0.5*AA*(pow(deltac/(sqrt(nu)*b),-a)+1.0)*exp(-c*nu*pow(deltac,-2));
+    ans= 0.5*AA*(1.0 + pow(deltac/(sqrt(nu)*b),-a))*exp(-c*nu*pow(deltac,-2));
    }
   if("Watson"==mb)
    {
@@ -128,23 +128,20 @@ real_prec MASS_BIAS_FUNCTIONS::dm_h_bias(real_prec z, real_prec nu, void *s_p){
   struct s_CosmologicalParameters * s_cp= (struct s_CosmologicalParameters *)s_p;
   Cosmology cf(*s_cp);
   string mb=s_cp->halo_mass_bias_fit;
-
   real_prec n_nu=sqrt(nu);
-
   real_prec deltac = cf.critical_overdensity(z);
-
   real_prec dcontrast=s_cp->Delta_SO;//    cf.density_contrast_top_hat(z,s_cp);
   real_prec ans=0;
   if(mb=="Peak_Background")ans= nu/(deltac);
-  if(mb=="Mo_White")ans= 1.+(0.75*nu-1)/(deltac); 
-  if(mb=="Sheth_Tormen")
+  else if(mb=="Mo_White")ans= 1.+(0.75*nu-1)/(deltac); 
+  else if(mb=="Sheth_Tormen")
   {
     a=0.707;
     b=0.35; //valores de Tinker 2005 0.35; valor original Sheth-Tormen 0.5
     c=0.80; //valores de Tinker 2005 0.80 ; valor original Sheth-Tormen 0.6
     ans= 1.+(1./((deltac)*sqrt(a)))*(sqrt(a)*(a*nu)+b*sqrt(a)*pow(a*nu,1.-c)-pow(a*nu,c)/(pow(a*nu,c)+b*(1.-c)*(1.-0.5*c)));
   }
-  if(mb=="Tinker") //https://arxiv.org/pdf/1001.3162.pdf
+  else if(mb=="Tinker") //https://arxiv.org/pdf/1001.3162.pdf
   {
     y=log10(dcontrast); 
     A=1.0+0.24*y*exp(-pow(4./y,4));
@@ -156,7 +153,7 @@ real_prec MASS_BIAS_FUNCTIONS::dm_h_bias(real_prec z, real_prec nu, void *s_p){
     ans= 1.0-A*pow(n_nu,a)/(pow(n_nu,a)+pow(deltac,a)) +  B*pow(n_nu,b) + C*pow(n_nu,c);
   }
   
-  if(mb=="Pillepich")
+  else if(mb=="Pillepich")
      ans = 0.647-0.540*pow(deltac/sqrt(nu),-1.0)+1.614*pow(deltac/sqrt(nu),-2.0);
   
 
