c096b9a6971a6c34e66b45b8765eccfbf00b74de
Andrés Balaguera Antolínez <abalant@gmail.com>
2025-09-04 18:10:00 +0100

    Add files via upload

--- codes/cosmic_web.cpp
+++ codes/cosmic_web.cpp
@@ -10,6 +10,98 @@
 using namespace std;
 ////////////////////////////////////////////////////////////////////////////
 
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+void get_hist(Params params, vector<real_prec>&dm_alpha, vector<real_prec>&tr_alpha, vector<real_prec>&alpha_bias, vector<ULONG>&cwc, bool fitype)
+{
+
+  int nb=200;
+  real_prec bmin=-2; real_prec bmax=2; real_prec delta=(bmax-bmin)/static_cast<real_prec>(nb);
+  for (int icwt=0; icwt<=4;++icwt)
+     {
+
+      vector<real_prec>hist(nb,0); 
+      vector<real_prec>hist_alphah(nb,0); 
+      vector<real_prec>hist_alphadm(nb,0); 
+
+       if(icwt==0)
+          {
+             for(ULONG i=0;i<dm_alpha.size();++i)
+               {
+                 int ibin=get_bin(log10(alpha_bias[i]),bmin,nb,delta,0);
+                 if(log10(alpha_bias[i])<bmax && log10(alpha_bias[i])>=bmin)
+                    hist[ibin]++;
+
+                  int abin=get_bin(log10(tr_alpha[i]),bmin,nb,delta,0);
+                  if(log10(tr_alpha[i])<bmax && log10(tr_alpha[i])>=bmin)
+                      hist_alphah[abin]++;
+
+                      
+                  int dbin=get_bin(log10(dm_alpha[i]),bmin,nb,delta,0);
+                  if(log10(dm_alpha[i])<bmax && log10(dm_alpha[i])>=bmin)
+                      hist_alphadm[dbin]++;
+
+
+                  }
+             }
+          else
+          {
+            for(ULONG i=0;i<dm_alpha.size();++i)
+            {
+              if(cwc[i]==icwt)
+                {
+                  int ibin=get_bin(log10(alpha_bias[i]),bmin,nb,delta,0);
+                  if(log10(alpha_bias[i])<bmax && log10(alpha_bias[i])>=bmin)
+                    hist[ibin]++;
+
+                  int abin=get_bin(log10(tr_alpha[i]),bmin,nb,delta,0);
+                  if(log10(tr_alpha[i])<bmax && log10(tr_alpha[i])>=bmin)
+                    hist_alphah[abin]++;
+
+                  int dbin=get_bin(log10(dm_alpha[i]),bmin,nb,delta,0);
+                  if(log10(dm_alpha[i])<bmax && log10(dm_alpha[i])>=bmin)
+                    hist_alphadm[dbin]++;
+  
+                    
+                }
+            }
+        }
+        string ftype = fitype == 0 ? "dm" : "tr";
+        string fileo=params._Output_directory()+"alpha_hbias_p"+to_string(params._unitsim_plabel())+"_cwt"+to_string(icwt)+"_"+ftype+".txt";
+        cout<<"Writting distribution of alpha bias to file "<<fileo<<endl;
+        ofstream off; off.open(fileo.c_str());
+        for(int i=0; i<nb; ++i)
+            off<<bmin+(i+0.5)*delta<<"\t"<<hist[i]/static_cast<real_prec>(dm_alpha.size())<<endl;
+        off.close();
+
+        fileo=params._Output_directory()+"alphah_hbias_p"+to_string(params._unitsim_plabel())+"_cwt"+to_string(icwt)+"_"+ftype+".txt";
+        cout<<"Writting distribution alpha bias to file "<<fileo<<endl;
+        off.open(fileo.c_str());
+        for(int i=0; i<nb; ++i)
+            off<<bmin+(i+0.5)*delta<<"\t"<<hist_alphah[i]/static_cast<real_prec>(dm_alpha.size())<<endl;
+        off.close();
+
+        fileo=params._Output_directory()+"alphadm_hbias_p"+to_string(params._unitsim_plabel())+"_cwt"+to_string(icwt)+"_"+ftype+".txt";
+        cout<<"Writting alpha bias to file "<<fileo<<endl;
+        off.open(fileo.c_str());
+        for(int i=0; i<nb; ++i)
+            off<<bmin+(i+0.5)*delta<<"\t"<<hist_alphadm[i]/static_cast<real_prec>(dm_alpha.size())<<endl;
+        off.close();
+      }
+
+
+
+}
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+
+
+
 
 ////////////////////////////////////////////////////////////////////////////
 void usage(string s)
@@ -22,8 +114,8 @@ void usage(string s)
   std::cout<<"\t\n\tCosmologicalCATalogs for LArge Scale Structure"<<endl;
   std::cout<<"\t\n\tHow to run: "<<s<<"\t [-option] [argument]"<<endl;
   std::cout<<"\t\n\tOptions: "<<endl;
-  std::cout<<"\t         -a for information on the author, no argument"<<endl;
-  std::cout<<"\t         -b parameter_file.ini: To perform cosmic/web analysis"<<endl;
+  std::cout<<"\t         -a for information on the author. No argument"<<endl;
+  std::cout<<"\t         -b parameter_file.ini: To perform cosmic-web analysis and bias assignment"<<endl;
   std::cout<<"\t         -h parameter_file.ini: Help"<<endl;
   std::cout<<"\t         -i parameter_file.ini: Shows input pars"<<endl;
   std::cout<<"\tConsult ../Headers/def.h for pre-procesor directives"<<endl;
@@ -43,7 +135,7 @@ int main(int argc, char *argv[])
   string par_file;
 
 #ifdef USE_GALAXY_TOOLS
-  throw std::invalid_argument("USE_GALAXY_TOOLS is defined, enable double prec everywhere. Plese desable and compile again.");
+  throw std::invalid_argument("USE_GALAXY_TOOLS is defined, enable double prec evecd da NaAaAAAArywhere. Plese desable and compile again.");
 #endif
   int NTHREADS = omp_get_max_threads();
   omp_set_num_threads(NTHREADS);
@@ -72,197 +164,132 @@ int main(int argc, char *argv[])
     {
       So.show_preproc();
     }
-    else if ( temp  == 'b') // assign bias to tracers.
+    else if ( temp  == 'b') // assign bias and do CW analysus using tracers and DM.
     {
-      FileOutput File;
+
+
+      FileOutput File;     // File management
       So.welcome_message_c();
-      par_file = argv[2];
-      Params params(par_file);
+      par_file = argv[2];  // Get parameter file
+      Params params(par_file);  // Read parameter file
 
       //Read dm field
-      vector<real_prec> dm_field(params._NGRID(), 0);
-      File.read_array(params._Input_Directory_X() + params._Name_Catalog_X(), dm_field);
+      vector<real_prec> dm_field(params._NGRID(), 0); //COntainer for dm
+      File.read_array(params._Input_Directory_X() + params._Name_Catalog_X(), dm_field); // Readin the bunary file
 
 
       // Read catalog
-      Catalog cat(params);
-      cat.read_catalog(params._Input_dir_cat() + params._file_catalogue(), 0);
+      Catalog cat(params); // Feed params into the Catlaog class and define an object of that type
+      cat.read_catalog(params._Input_dir_cat() + params._file_catalogue(), 0); // Read the asciii file
+
+      //----------------------------------------------
       // Get density field from tracers
+      //----------------------------------------------
+ 
+      vector<real_prec> tr_field_counts(params._NGRID(), 0); // Cointainer for counts
+      cat.get_density_field_grid(_COUNTS_,tr_field_counts);  // Computing counts
 
+      //Assign bias to tracers and get the bias field:
 
-      vector<real_prec> tr_field_counts(params._NGRID(), 0);
-      cat.get_density_field_grid(_COUNTS_,tr_field_counts);
-      //Assign bias to tracers and get the bias field
+      // Definr an object of type Power Spectrum, passing the parametes through the class variable param
       PowerSpectrumF power(params);
+
+      //Assign individual bias to objhects in cat.Halo. That bias uses the dm_field.
       power.object_by_object_bias(cat.Halo, dm_field);
 
-      vector<real_prec> bias_field(params._NGRID(), 0);
-      cat.get_density_field_grid(_COUNTS_,tr_field_counts,bias_field);
-      string fileb=params._Output_directory()+"ls_bias_p"+to_string(params._unitsim_plabel());
-      File.write_array(fileb,bias_field);
-      bias_field.clear();bias_field.shrink_to_fit();
-  
+      vector<real_prec> bias_field(params._NGRID(), 0); // Define container to allocate the bias on a mesh
+       
+      cat.get_density_field_grid(_COUNTS_,tr_field_counts,bias_field); //Get halo bias averaged on a mesh.
 
-      string fileo;
-      vector<real_prec> tr_field(params._NGRID(), 0);
-      cat.get_density_field_grid(_iBIAS_,tr_field);
+      string fileb=params._Output_directory()+"ls_bias_p"+to_string(params._unitsim_plabel()); //Defile output file
+
+      File.write_array(fileb,bias_field); //Write the bias on the mesh to poutput file (binary)
+
+      bias_field.clear();bias_field.shrink_to_fit();// Release memmory
+ 
+
+      vector<real_prec> tr_field(params._NGRID(), 0); //container for
+      cat.get_density_field_grid(_iBIAS_,tr_field);  
       cat.get_density_field_grid(_COUNTS_,tr_field);
-      fileo=params._Output_directory()+"field_hbias_p"+to_string(params._unitsim_plabel());
+      string fileo=params._Output_directory()+"field_hbias_p"+to_string(params._unitsim_plabel());
 
       File.write_array(fileo,tr_field);
 
 
+      //-----------------------------
+      // Define Cwc cobject to perform cosmic-web analysis based on the halo density field.  
       Cwclass cwclass(params); // for halos
 
-      // Get tidal anisotropy from tracers
-      vector<real_prec> tr_alpha(params._NGRID(), 0);
-      cwclass.get_tidal_anisotropy(tr_field, tr_alpha);
-
+      // Get tidal anisotropy (TA hereafter) from tracers
+      vector<real_prec> tr_alpha(params._NGRID(), 0); // Container for TA computed from the halo tidal field
+      cwclass.get_tidal_anisotropy(tr_field, tr_alpha); // Compute the TA halo field --tr_field-- and allocate in tr_alpha
 
       fileo=params._Output_directory()+"alpha_hbias_p"+to_string(params._unitsim_plabel());
-      File.write_array(fileo,tr_alpha);
+      File.write_array(fileo,tr_alpha); //Write tidal anisotropy
+
       fileo=params._Output_directory()+"l1_hbias_p"+to_string(params._unitsim_plabel());
-      File.write_array(fileo,cwclass.lambda1);
+      File.write_array(fileo,cwclass.lambda1); //Write first eigenvalue of halo tidal field
+
+
       fileo=params._Output_directory()+"l2_hbias_p"+to_string(params._unitsim_plabel());
-      File.write_array(fileo,cwclass.lambda2);
+      File.write_array(fileo,cwclass.lambda2); //Write second eigenvalue of halo tidal field
+
       fileo=params._Output_directory()+"l3_hbias_p"+to_string(params._unitsim_plabel());
-      File.write_array(fileo,cwclass.lambda3);
+      File.write_array(fileo,cwclass.lambda3); //Write third eigenvalue of halo tidal field
 
       fileo=params._Output_directory()+"cwc_hbias_p"+to_string(params._unitsim_plabel());
-      File.write_array(fileo,cwclass.CWClass);
+      File.write_array(fileo,cwclass.CWClass);  //Write the cwc of each cell, computed in from tracers in CWClass.
 
-      vector<ULONG> cwc(params._NGRID(),0);
 
+      //-----------------------------
+      //-----------------------------
       // Get tidal anisotropy from dm
 
-      vector<real_prec> dm_alpha(params._NGRID(), 0);
-      Cwclass cwclass_dm(params); //para dm
+      vector<real_prec> dm_alpha(params._NGRID(), 0); //Coitainer for TA computed from the tidal field of dm
+      Cwclass cwclass_dm(params); //CWC type for dark matter
 
-      cwclass_dm.get_tidal_anisotropy(dm_field, dm_alpha);
+      cwclass_dm.get_tidal_anisotropy(dm_field, dm_alpha); //Compute tidal anisotropy from dm
+
       fileo=params._Output_directory()+"alpha_dm_p"+to_string(params._unitsim_plabel());
-      File.write_array(fileo,dm_alpha);
+      File.write_array(fileo,dm_alpha); // Write TA from dm to binary
 
-      vector<real_prec> alpha_bias(params._NGRID(), 0);
+      vector<real_prec> alpha_bias(params._NGRID(), 0); // Container for the bias--ratio-- of TA
+
+#ifdef _USE_OMP_
 #pragma omp parallel for
-    for(ULONG i=0;i<dm_field.size();++i)
+#endif
+  for(ULONG i=0;i<dm_field.size();++i) // get the ratio between TA from tr and TA from dm
       alpha_bias[i]=tr_alpha[i]/dm_alpha[i];
 
 
-
       fileo=params._Output_directory()+"bias_alpha_p"+to_string(params._unitsim_plabel());
-      File.write_array(fileo,alpha_bias);
+      File.write_array(fileo,alpha_bias); //Write the ratio of TA's
+
       fileo=params._Output_directory()+"l1_dm_p"+to_string(params._unitsim_plabel());
-      File.write_array(fileo,cwclass_dm.lambda1);
+      File.write_array(fileo,cwclass_dm.lambda1); //Write first eigenvalue of DM tidal field
+
       fileo=params._Output_directory()+"l2_dm_p"+to_string(params._unitsim_plabel());
-      File.write_array(fileo,cwclass_dm.lambda2);
+      File.write_array(fileo,cwclass_dm.lambda2);//Write second eigenvalue of DM tidal field
+
       fileo=params._Output_directory()+"l3_dm_p"+to_string(params._unitsim_plabel());
-      File.write_array(fileo,cwclass_dm.lambda3);
+      File.write_array(fileo,cwclass_dm.lambda3);//Write third eigenvalue of DM tidal field
+
       fileo=params._Output_directory()+"cwc_dm_p"+to_string(params._unitsim_plabel());
-      File.write_array(fileo,cwclass_dm.CWClass);
-
-      // Asigna el cwc al ultimo calculado:
+      File.write_array(fileo,cwclass_dm.CWClass);  //Write the cwc of each cell, computed in from tracers in CWClass.
 
 
-      // Here we rank orde the eigenvalues from the tracer tidal foeld to those form the dm
-//      calc_pdf("log", params._NGRID(),NXn, this->Xmax, this->Xmin, this->delta_X_REF_PDF, this->pdf_ref);
-//      rankorder(ZERO, xbaux, NXn,  this->Xmax, this->Xmin, delta_dm_aux_mem, pdf_in, this->pdf_ref);
+      // Histograms here are done classifying CW according to dm.
+      get_hist(params, dm_alpha, tr_alpha, alpha_bias,cwclass_dm.CWClass, 0);
 
+      // Histograms here are done classifying CW according to tracers.
+      get_hist(params, dm_alpha, tr_alpha, alpha_bias,cwclass.CWClass, 1);
 
-
-
-
-      //cwc=cwclass.CWClass; // halos
-      cwc=cwclass_dm.CWClass; // dm
-
-      
-
-      // Calculo de Histogramas
-
-
-       int nb=200;
-       real_prec bmin=-2; real_prec bmax=2; real_prec delta=(bmax-bmin)/static_cast<real_prec>(nb);
-
-       for (int icwt=0; icwt<=4;++icwt)
-        {
-          vector<real_prec>hist(nb,0); 
-          vector<real_prec>hist_alphah(nb,0); 
-          vector<real_prec>hist_alphadm(nb,0); 
-
-          if(icwt==0)
-            {
-              for(ULONG i=0;i<dm_field.size();++i)
-                {
-                  int ibin=get_bin(log10(alpha_bias[i]),bmin,nb,delta,0);
-                  if(log10(alpha_bias[i])<bmax && log10(alpha_bias[i])>=bmin)
-                    hist[ibin]++;
-
-                  int abin=get_bin(log10(tr_alpha[i]),bmin,nb,delta,0);
-                  if(log10(tr_alpha[i])<bmax && log10(tr_alpha[i])>=bmin)
-                      hist_alphah[abin]++;
-
-                      
-                  int dbin=get_bin(log10(dm_alpha[i]),bmin,nb,delta,0);
-                  if(log10(dm_alpha[i])<bmax && log10(dm_alpha[i])>=bmin)
-                      hist_alphadm[dbin]++;
-
-
-                  }
-             }
-          else
-          {
-            for(ULONG i=0;i<dm_field.size();++i)
-            {
-              if(cwc[i]==icwt)
-                {
-                  int ibin=get_bin(log10(alpha_bias[i]),bmin,nb,delta,0);
-                  if(log10(alpha_bias[i])<bmax && log10(alpha_bias[i])>=bmin)
-                    hist[ibin]++;
-
-                  int abin=get_bin(log10(tr_alpha[i]),bmin,nb,delta,0);
-                  if(log10(tr_alpha[i])<bmax && log10(tr_alpha[i])>=bmin)
-                    hist_alphah[abin]++;
-
-                  int dbin=get_bin(log10(dm_alpha[i]),bmin,nb,delta,0);
-                  if(log10(dm_alpha[i])<bmax && log10(dm_alpha[i])>=bmin)
-                    hist_alphadm[dbin]++;
-  
-                    
-                }
-            }
-        }
-        fileo=params._Output_directory()+"bias_alpha_hbias_p"+to_string(params._unitsim_plabel())+"_cwt"+to_string(icwt)+".txt";
-        cout<<"Writting alpha bias to file "<<fileo<<endl;
-        ofstream off; off.open(fileo.c_str());
-        for(int i=0; i<nb; ++i)
-            off<<bmin+(i+0.5)*delta<<"\t"<<hist[i]/static_cast<real_prec>(dm_field.size())<<endl;
-        off.close();
-
-        fileo=params._Output_directory()+"alphah_hbias_p"+to_string(params._unitsim_plabel())+"_cwt"+to_string(icwt)+".txt";
-        cout<<"Writting alpha bias to file "<<fileo<<endl;
-        off.open(fileo.c_str());
-        for(int i=0; i<nb; ++i)
-            off<<bmin+(i+0.5)*delta<<"\t"<<hist_alphah[i]/static_cast<real_prec>(dm_field.size())<<endl;
-        off.close();
-
-        fileo=params._Output_directory()+"alphadm_hbias_p"+to_string(params._unitsim_plabel())+"_cwt"+to_string(icwt)+".txt";
-        cout<<"Writting alpha bias to file "<<fileo<<endl;
-        off.open(fileo.c_str());
-        for(int i=0; i<nb; ++i)
-            off<<bmin+(i+0.5)*delta<<"\t"<<hist_alphadm[i]/static_cast<real_prec>(dm_field.size())<<endl;
-        off.close();
-
-
-
-
-        }
-
+     //Relesas memmory (not requested as memmory is clean when this if ends)   
      dm_alpha.clear(); dm_alpha.shrink_to_fit();
-
-     dm_field.clear();
-     dm_field.shrink_to_fit();
-     tr_field.clear();
-     tr_field.shrink_to_fit();
-
+     dm_field.clear(); dm_field.shrink_to_fit();
+     tr_field.clear(); tr_field.shrink_to_fit();
+     
+     // End of the '-b' option
 
     }
     else if ('?' == temp)
